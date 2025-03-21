8beaefb0fb53778ffde4c34d4d75a32e4744627c
Maisa Liimatta <maisa.liimatta@metropolia.fi>
2025-03-21 10:50:41 +0200

    completed tehtävä 1

--- 9. Objects/tehtava1.py
+++ 9. Objects/tehtava1.py
@@ -1,10 +1,3 @@
-#Write a Car class that has the following properties:
-# registration number, maximum speed, current speed and travelled distance.
-# Add a class initializer that sets the first two of the properties based on
-# parameter values. The current speed and travelled distance of a new car must
-# be automatically set to zero. Write a main program where you create a new car
-# (registration number ABC-123, maximum speed 142 km/h). Finally, print out all
-# the properties of the new car.
 class Car:
     def __init__(self, reg_number, max_speed):
         self.reg_number = reg_number
