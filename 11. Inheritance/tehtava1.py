963a52cdd2a1392f66c6d56595d22f2eecd21ed2
Maisa Liimatta <maisa.liimatta@metropolia.fi>
2025-04-09 19:58:48 +0300

    tehtävä 1 ja 2 valmis

---
+++ 11. Inheritance/tehtava1.py
@@ -0,0 +1,31 @@
+class Publication:
+    def __init__(self, name):
+        self.name = name
+class Book(Publication):
+    def __init__(self, name, author, page_count):
+        super().__init__(name)
+        self.author = author
+        self.page_count = page_count
+    def print_information(self):
+        print(f"Book: {self.name}")
+        print(f"Author: {self.author}")
+        print(f"Page count: {self.page_count}")
+#
+class Magazine(Publication):
+    def __init__(self, name, chief_editor):
+        super().__init__(name)
+        self.chief_editor = chief_editor
+    def print_information(self):
+        print(f"Magazine: {self.name}")
+        print(f"Chief Editor: {self.chief_editor}")
+#
+def main():
+    donald_duck = Magazine("Donald Duck", "Aki Hyyppä")
+    compartment_no_6 = Book("Compartment No. 6", "Rosa Liksom", 192)
+#
+    donald_duck.print_information()
+    print()
+    compartment_no_6.print_information()
+#
+if __name__ == "__main__":
+    main()
---
+++ 11. Inheritance/tehtava2.py
@@ -0,0 +1,38 @@
+import random
+class Car:
+    def __init__(self, reg_number, max_speed):
+        self.reg_number = reg_number
+        self.max_speed = max_speed
+        self.travelled_dis = 0
+        self.current_speed = 0
+    def accelerate(self, speed_change):
+        self.current_speed += speed_change
+        if self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+    def drive(self, hours_driven):
+        self.travelled_dis += hours_driven * self.current_speed
+class ElectricCar(Car):
+    def __init__(self, reg_number, max_speed, battery_capacity):
+        super().__init__(reg_number, max_speed)
+        self.battery_capacity = battery_capacity
+class GasolineCar(Car):
+    def __init__(self, reg_number, max_speed, tank_volume):
+        super().__init__(reg_number, max_speed)
+        self.tank_volume = tank_volume
+#
+def main():
+    electric_car = ElectricCar("ABC-15", 180, 52.5)  # reg_number, max_speed, battery_capacity
+    gasoline_car = GasolineCar("ACD-123", 165, 32.3)  # reg_number, max_speed, tank_volume
+#
+    electric_car.accelerate(random.randint(0, electric_car.max_speed))
+    electric_car.drive(3)
+    gasoline_car.accelerate(random.randint(0, gasoline_car.max_speed))
+    gasoline_car.drive(3)
+#
+    print(f"Electric Car {electric_car.reg_number} traveled {electric_car.travelled_dis} km.")
+    print(f"Gasoline Car {gasoline_car.reg_number} traveled {gasoline_car.travelled_dis} km.")
+#
+if __name__ == "__main__":
+    main()
