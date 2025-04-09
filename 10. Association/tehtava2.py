a06cdbe7b9026b8738cbbd801365949d755d61e7
Maisa Liimatta <maisa.liimatta@metropolia.fi>
2025-04-09 19:25:59 +0300

    tehtävä 2 valmis

---
+++ 10. Association/tehtava2.py
@@ -0,0 +1,37 @@
+
+class elevator:
+    def __init__(self, top_floor, bottom_floor, elevator_number):
+        self.top_floor = top_floor
+        self.bottom_floor = bottom_floor
+        self.current_floor = bottom_floor
+        self.elevator_number = elevator_number
+#
+    def floor_up(self):
+        self.current_floor += 1
+#
+    def floor_down(self):
+        self.current_floor -= 1
+#
+    def go_to_floor(self, target_floor):
+        while self.current_floor != target_floor:
+            if target_floor > self.current_floor:
+                self.floor_up()
+            elif target_floor < self.current_floor:
+                self.floor_down()
+        print(f"Elevator {self.elevator_number} is now on floor {self.current_floor}.")
+#
+class building:
+    def __init__(self, top_floor, bottom_floor, elevators):
+        self.top_floor = top_floor
+        self.bottom_floor = bottom_floor
+        self.elevators = [elevator(top_floor, bottom_floor, _ + 1)
+        for _ in range(elevators)]
+    def run_elevator(self, elevator_number, target_floor):
+        elevator = self.elevators[elevator_number - 1]
+        elevator.go_to_floor(target_floor)
+#
+rakennus = building(100, 1, 3)
+rakennus.run_elevator(1, 10)
+rakennus.run_elevator(2,50)
+rakennus.run_elevator(3,2)
+
